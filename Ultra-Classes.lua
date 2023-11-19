ec2f360990ad20c6790180899fb6acb2b00938d8
kgukmz <144381271+kgukmz@users.noreply.github.com>
2023-11-19 20:10:38 +0000

    Create Ultra-Classes.lua

---
+++ Ultra-Classes.lua
@@ -0,0 +1,74 @@
+return {
+    --Dragon Sage
+    "Lightning Elbow",
+    "Lightning Drop",
+    "DragonDash",
+    --Oni
+    "Demon Flip",
+    "Demon Step",
+    "Axe Kick",
+    "Misogi",
+    --Deepknight
+    "Deep Sacrifice",
+    "Leviathan Plunge",
+    --Dragon Slayer
+    "Dragon Awakening",
+    "Thunder Spear Crash",
+    "Wing Soar",
+    --Necromancer
+    "Command Monsters",
+    "Furantur",
+    "Howler",
+    "Secare",
+    --Illusionist (ULTRA)
+    "Dominus",
+    "Globus",
+    "Intermissum",
+    "IllusionMasterSkills",
+    --Illusionist (SUPER, FOR OBSERVE DETECTION)
+    "Observe",
+    "Custos",
+    "Claritum",
+    --Druid
+    "Perflora",
+    "Floresco",
+    "Nature Mastery",
+    "Poison Soul",
+    --Shinobi
+    "Grapple",
+    "Owl Slash",
+    "Shadowrush",
+    "Resurrection",
+    --Faceless
+    "Ethereal Strike",
+    "Shadow Fan",
+    "Chain Lethality",
+    "ShadowDash",
+    "UpgradedBane",
+    --Whisperer
+    "Elegant Slash",
+    "Needle's Eye",
+    "RapierTraining",
+    --Cadence
+    "Joyous Dance",
+    "Song of Lethargy",
+    "Sweet Soothing",
+    --Abysswalker
+    "Abyssal Scream",
+    "Wrathful Leap",
+    "Accursed",
+    "Abysswalker",
+    --Wraith Knight
+    "Dark Eruption",
+    "RuneShield",
+    "MasteredRune",
+    "Dark Flame Burst",
+    "Dark Sigil Helmet",
+    --Lapidarist
+    "Gemcutter",
+    "Shardcaster",
+    "Sharpener",
+    "Charged Blow",
+    "Hyper Body",
+    "MythicStability",
+}
